6cd0352fe922613aec414c7ea4a7b5755e1a3e60
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-07-22 00:13:24 +0100

    Completed Bot Logic ✅

--- new-agent-tracker/src/agent.ts
+++ new-agent-tracker/src/agent.ts
@@ -1,38 +1,49 @@
-import { BigNumber } from "ethers";
 import {
-  BlockEvent,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
   FindingType,
 } from "forta-agent";
 
-export const CREATE_AGENT_FUNCTION_SIGNATURE = "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
-export const NETHERMIND_DEPLOYER_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
-export const FORTA_DEPLOY_CONTRACT = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
+export const CREATE_AGENT_FUNCTION_SIGNATURE =
+  "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
+export const NETHERMIND_DEPLOYER_ADDRESS =
+  "0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8";
+export const FORTA_DEPLOY_CONTRACT =
+  "0x61447385b019187daa48e91c55c02af1f1f3f863";
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = [];
-
-  const createAgentFunctionCalls = txEvent.filterFunction(CREATE_AGENT_FUNCTION_SIGNATURE, FORTA_DEPLOY_CONTRACT);
-  console.log(createAgentFunctionCalls);
+  const createAgentFunctionCalls = txEvent.filterFunction(
+    CREATE_AGENT_FUNCTION_SIGNATURE,
+    FORTA_DEPLOY_CONTRACT
+  );
   createAgentFunctionCalls.forEach((createAgentCall) => {
-    console.log(createAgentCall);
-  })
+    if (
+      txEvent.to == FORTA_DEPLOY_CONTRACT &&
+      txEvent.from == NETHERMIND_DEPLOYER_ADDRESS
+    ) {
+      const [, , metadata] = createAgentCall.args;
+      const findingObject = {
+        name: "New agent",
+        description: "Bot Deployed By Nethermind",
+        alertId: "NETHERMIND-1",
+        protocol: "NETHERMIND",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: {
+          metadata,
+        },
+      };
+      findings.push(Finding.fromObject(findingObject));
+    }
+  });
   return findings;
 };
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
-
 export default {
   handleTransaction,
-  // handleBlock
 };
