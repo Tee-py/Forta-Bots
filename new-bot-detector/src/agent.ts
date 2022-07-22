565b8e39c8c9a2b8e768fa7959c92f7f958c29b5
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-07-22 14:39:33 +0100

    chore: code formatting

--- new-bot-detector/src/agent.ts
+++ new-bot-detector/src/agent.ts
@@ -1,28 +1,12 @@
 import { LogDescription, TransactionDescription } from "ethers/lib/utils";
-import {
-  Finding,
-  HandleTransaction,
-  TransactionEvent,
-  FindingSeverity,
-  FindingType,
-} from "forta-agent";
+import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
 
-import {
-  CREATE_AGENT_FUNCTION_SIGNATURE,
-  FORTA_DEPLOY_CONTRACT,
-  NETHERMIND_DEPLOYER_ADDRESS,
-} from "./constants";
+import { CREATE_AGENT_FUNCTION_SIGNATURE, FORTA_DEPLOY_CONTRACT, NETHERMIND_DEPLOYER_ADDRESS } from "./constants";
 
-function provideTransactionHandler(
-  deployer: string,
-  forta_contract: string
-): HandleTransaction {
+function provideTransactionHandler(deployer: string, forta_contract: string): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
-    const createAgentFunctionCalls = txEvent.filterFunction(
-      CREATE_AGENT_FUNCTION_SIGNATURE,
-      forta_contract
-    );
+    const createAgentFunctionCalls = txEvent.filterFunction(CREATE_AGENT_FUNCTION_SIGNATURE, forta_contract);
     createAgentFunctionCalls.forEach((call: TransactionDescription) => {
       if (txEvent.from == deployer.toLocaleLowerCase()) {
         const [agentId, owner, metadata, chainIds] = call.args;
@@ -48,8 +32,5 @@ function provideTransactionHandler(
 }
 
 export default {
-  handleTransaction: provideTransactionHandler(
-    NETHERMIND_DEPLOYER_ADDRESS,
-    FORTA_DEPLOY_CONTRACT
-  ),
+  handleTransaction: provideTransactionHandler(NETHERMIND_DEPLOYER_ADDRESS, FORTA_DEPLOY_CONTRACT),
 };
