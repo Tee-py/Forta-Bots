c8cff77d2ed5cd042dbb43cae2d53296795f236c
Emmanuel Oluwatobi <emmanueloluwatobi2000@gmail.com>
2022-08-05 17:23:53 +0100

    refac: refactored agent.ts

--- uniswapv3-swap-detector/src/agent.ts
+++ uniswapv3-swap-detector/src/agent.ts
@@ -5,27 +5,35 @@ import {
   HandleTransaction,
   TransactionEvent,
 } from "forta-agent";
-import { SWAP_EVENT, V3_FACTORY_CONTRACT_ADDRESS } from "./constants";
+import { UNISWAP_V3_POOL_ABI, V3_FACTORY_CONTRACT_ADDRESS } from "./constants";
 import { createSwapFinding, createSwapMetaData, isUniSwapPool } from "./utils";
 import LRU from "lru-cache";
 
-export function provideTransactionHandler(factoryAddress: string, provider: JsonRpcProvider): HandleTransaction {
+export function provideTransactionHandler(factoryAddress: string, provider: JsonRpcProvider, cache: LRU<string, boolean>): HandleTransaction {
   return async (txEvent: TransactionEvent): Promise<Finding[]> => {
     const findings: Finding[] = [];
-    const swaps = txEvent.filterLog(SWAP_EVENT);
-    const uniSwapPoolCache = new LRU<string, boolean>({ max: 500 });
+    const swaps = txEvent.filterLog(UNISWAP_V3_POOL_ABI[0]);
     for (const swapEvent of swaps) {
       const pairAddress = swapEvent["address"];
-      const isValidPool = await isUniSwapPool(factoryAddress, pairAddress, uniSwapPoolCache, provider);
+      let isValidPool;
+      try {
+        isValidPool = await isUniSwapPool(factoryAddress, pairAddress, cache, provider);
+      } catch (error) {
+        return []
+      }
+
       if (isValidPool){
-        const metadata = createSwapMetaData(swapEvent.args);
+        const metadata = createSwapMetaData(swapEvent.args, pairAddress);
         findings.push(createSwapFinding(metadata));
       }
     }
-    return findings
+    return findings;
   }
 }
 
+const provider = new JsonRpcProvider(getJsonRpcUrl());
+const uniSwapPoolCache = new LRU<string, boolean>({ max: 500 })
+
 export default {
-  handleTransaction: provideTransactionHandler(V3_FACTORY_CONTRACT_ADDRESS, new JsonRpcProvider(getJsonRpcUrl())),
+  handleTransaction: provideTransactionHandler(V3_FACTORY_CONTRACT_ADDRESS, provider, uniSwapPoolCache),
 };
